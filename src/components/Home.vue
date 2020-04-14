978196ded7573f812941d5b97833e516c0ca0f50
athallara <athallarizk@gmail.com>
2020-04-14 22:33:01 +0700

    project init

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="Home">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure ipsam, laudantium sit voluptates, beatae dolorum fuga deleniti et in commodi dolore temporibus pariatur quia magni eos assumenda consectetur quae aperiam!
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
